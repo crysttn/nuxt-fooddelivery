431610512635491976f49804bf55b7afd44b969d
Crys T. T. Nguyen <crysttnguyen@gmail.com>
2020-01-16 20:58:50 +0100

    Changed apiUrl in Strapi config

--- utils/Strapi.js
+++ utils/Strapi.js
@@ -1,6 +1,6 @@
 import Strapi from 'strapi-sdk-javascript/build/main'
 
-const apiUrl = process.env.API_URL || 'http://localhost:1337'
+const apiUrl = process.env.API_URL || 'https://hidden-springs-04285.herokuapp.com/'
 const strapi = new Strapi(apiUrl)
 
 export default strapi;
